3d0f069e255f460d173187c6a9a3f56df48959d3
Setnour6 <70764728+Setnour6@users.noreply.github.com>
2022-11-30 22:47:25 -0500

    nvm

--- Content/CraftableTreasureBagsCalamityRecipes.cs
+++ Content/CraftableTreasureBagsCalamityRecipes.cs
@@ -196,7 +196,7 @@ namespace CraftableTreasureBags
 					.AddRecipeGroup("CraftableTreasureBags:Adamantite/Titanium Pendant")
 					.AddIngredient(CalamityMod, "PearlofEnthrallment")
 					.AddIngredient(CalamityMod, "LeviathanAmbergris")
-					.AddIngredient(CalamityMod, "TheCommunity")
+					//.AddIngredient(CalamityMod, "TheCommunity")
 					.AddIngredient(CalamityMod, "AquaticHeart")
 					.AddIngredient(CalamityMod, "LeviathanMask")
 					.AddIngredient(CalamityMod, "AnahitaMask")
